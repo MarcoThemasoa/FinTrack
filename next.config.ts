67b7b24d0582aaf3bf5e61ed2e260e87d9914e7d
Marco <phileo0221@gmail.com>
2025-05-21 07:29:46 +0000

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
-/** @type {import('next').NextConfig} */
-    const nextConfig = {
-      output: 'export', // Add this line
-    };
-
-    module.exports = nextConfig;
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
